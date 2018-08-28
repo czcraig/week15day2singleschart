2972a5db393d697ed3c03e078ee4eef43952c8a8
Chris Craig <ctc2@live.co.uk>
2018-08-28 22:51:34 +0100

    created files and started making containter

---
+++ src/containers/ItunesContainer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Album from '../components/Album.js'
+import AlbumChart from '../components/AlbumChart.js'
+
+
+class ItunesContainer extends React.Component {
+constructor(props){
+  super(props)
+  this.start = {
+    album: []
+  };
+}
+
+componentDidMount(){
+  this.url
+}
+
+render(){
+  
+}
+
+}
+
+
+export default ItunesContainer;

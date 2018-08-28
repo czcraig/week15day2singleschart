3ff08977738df7a07f52bf78111604e92d85bef5
Chris Craig <ctc2@live.co.uk>
2018-08-29 00:03:46 +0100

    updated file names

--- src/containers/ItunesContainer.js
+++ src/containers/ItunesContainer.js
@@ -1,25 +1,40 @@
 import React from 'react';
-import Album from '../components/Album.js'
-import AlbumChart from '../components/AlbumChart.js'
+import Single from '../components/Single.js'
+import SinglesChart from '../components/SinglesChart.js'
 
 
 class ItunesContainer extends React.Component {
 constructor(props){
   super(props)
   this.start = {
-    album: []
+    songs: []
   };
 }
 
 componentDidMount(){
-  this.url
+  const url = 'https://itunes.apple.com/gb/rss/topsongs/limit=20/json';
+  fetch(url)
+  .then((res)=> {
+    return res.json();
+  })
+  .then((single) => {
+    this.setState({ song: single.feed.entry});
+    console.log(this.state.song);
+  })
+  .catch((error)=> {
+    console.error(error);
+  });
 }
 
-render(){
-  
-}
 
+  render(){
+  return (
+    <div>
+      <h1>Singles Chart</h1>
+      <SinglesChart songs={this.state.songs}/>
+    </div>
+  );
 }
-
+}
 
 export default ItunesContainer;
